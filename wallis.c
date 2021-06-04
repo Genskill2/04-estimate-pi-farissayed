63e8273eb405c2551198b5b1707bf2e7a2a9815c
Faris Sayed <84718381+farissayed@users.noreply.github.com>
2021-06-04 19:59:59 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -2,39 +2,61 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#define random rand
+#define srandom srand
 
-float mc_pi(int n){
-  float x,y;
-  int i=0,circle=0;
-  float z;
-  int square=0;
+
+float mc_pi(int);
+
+float frandom() {
+  long int q = random();
+  float ret = (float)q/(float)RAND_MAX;
+  return ret;
+}
+
+int main(void) {
+  float pi0;
+  float pi1;
   
-  float pi;
+  pi0 = mc_pi(25000);
+  pi1 = mc_pi(25000);
+  printf("%f %f\n", pi0, pi1);
   
-  while (i<n-1){
-   
-   x=frandom();
-   y=frandom();
-   z=x*x+y*y;
-  
-   
-   if(z<=1){
-     circle+=1;
-     square+=1;
-     }
-   else{
-     square+=1;
-   
-   i++;
-     
-   }
-   
-   }
- 
- pi=(float)4*circle/square;
- return pi;
-   
+  if (pi0 == pi1) {
+      printf("Two separate estimates of pi are exactly the same. This is unlikely.\n");
+      abort();
+    }
+
+  if (fabs(pi0 - pi1) > 0.05) {
+      printf("Two separate estimates %f and %f are too different.\n", pi0, pi1);
+      abort();
+  }
+
     
-  
-  
+  for (int i=2000; i<5000; i++) {
+    pi0 = mc_pi(i);
+    if (!(fabs(pi0 - M_PI) < 0.4)) {
+      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi0);
+      abort();
+    }
+  }
+}
+
+float mc_pi(int n)
+{  float pi,x,y,z;
+int count=0;
+    for(int i = 0; i < n; ++i) {
+
+     x = frandom();
+
+     y = frandom();
+
+     z = x * x + y * y;
+
+     if( z <= 1 ) count++;
+ }
+
+ pi = (double) count / n * 4;
+
+ return pi;
 }
